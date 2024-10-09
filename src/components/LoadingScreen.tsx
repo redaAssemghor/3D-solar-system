64149b0d83281f944261435bea4fb7b27e1ddef1
redaAssemghor <assemghor.reda@gmail.com>
2024-10-09 18:21:11 +0100

    loading

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -4,7 +4,7 @@ const LoadingComponent = () => {
   return (
     <div
       className="flex items-center justify-center h-screen bg-no-repeat bg-center overflow-hidden z-10"
-      style={{ backgroundImage: `url('/assets/loading.jpg')` }}
+      style={{ backgroundImage: `url('/assets/galaxy.webp')` }}
     >
       <Loader />
     </div>
